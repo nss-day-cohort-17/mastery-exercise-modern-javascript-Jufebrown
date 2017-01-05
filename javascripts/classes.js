e0e8aa257e205a96ed8e2d2686afda4fc5b24e5e
Jufebrown <jufebrown@gmail.com>
2017-01-05 15:34:07 -0600

    multiplied health of all classes by 10

--- javascripts/classes.js
+++ javascripts/classes.js
@@ -25,7 +25,7 @@ RBDome.Stable.Drone.prototype = new RBDome.Stable.Robot();
 
 RBDome.Stable.FixedWing = function() {
   this.name = "FixedWing";
-  this.health = Math.floor((Math.random() * 50) + 20)
+  this.health = Math.floor((Math.random() * 50) + 200)
   this.damage = Math.floor((Math.random() * 50) + 200)
 };
 RBDome.Stable.FixedWing.prototype = new RBDome.Stable.Drone();
@@ -33,7 +33,7 @@ RBDome.Stable.FixedWing.prototype = new RBDome.Stable.Drone();
 
 RBDome.Stable.Rotor = function() {
   this.name = "Rotor";
-  this.health = Math.floor((Math.random() * 50) + 30)
+  this.health = Math.floor((Math.random() * 50) + 300)
   this.damage = Math.floor((Math.random() * 50) + 150)
 };
 RBDome.Stable.Rotor.prototype = new RBDome.Stable.Drone();
@@ -54,7 +54,7 @@ RBDome.Stable.Bipedal.prototype = new RBDome.Stable.Robot();
 
 RBDome.Stable.Scout = function() {
   this.name = "Scout";
-  this.health = Math.floor((Math.random() * 50) + 80)
+  this.health = Math.floor((Math.random() * 50) + 800)
   this.damage = Math.floor((Math.random() * 50) + 80)
 };
 RBDome.Stable.Scout.prototype = new RBDome.Stable.Bipedal();
@@ -62,7 +62,7 @@ RBDome.Stable.Scout.prototype = new RBDome.Stable.Bipedal();
 
 RBDome.Stable.ShockTroop = function() {
   this.name = "ShockTroop";
-  this.health = Math.floor((Math.random() * 50) + 100)
+  this.health = Math.floor((Math.random() * 50) + 1000)
   this.damage = Math.floor((Math.random() * 50) + 100)
 };
 RBDome.Stable.ShockTroop.prototype = new RBDome.Stable.Bipedal();
@@ -82,14 +82,14 @@ RBDome.Stable.Tank.prototype = new RBDome.Stable.Robot();
 
 RBDome.Stable.MBT = function() {
   this.name = "MBT";
-  this.health = Math.floor((Math.random() * 50) + 200)
+  this.health = Math.floor((Math.random() * 50) + 2000)
   this.damage = Math.floor((Math.random() * 50) + 200)
 };
 RBDome.Stable.MBT.prototype = new RBDome.Stable.Tank();
 
 RBDome.Stable.IFV = function() {
   this.name = "IFV";
-  this.health = Math.floor((Math.random() * 50) + 150)
+  this.health = Math.floor((Math.random() * 50) + 1500)
   this.damage = Math.floor((Math.random() * 50) + 100)
 };
 RBDome.Stable.IFV.prototype = new RBDome.Stable.Tank();
