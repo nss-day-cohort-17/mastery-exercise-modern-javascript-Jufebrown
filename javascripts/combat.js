61dbd15812342698b311cfad71d9330c40784a21
Jufebrown <jufebrown@gmail.com>
2017-01-05 15:34:45 -0600

    updated combat function

--- javascripts/combat.js
+++ javascripts/combat.js
@@ -1,10 +1,10 @@
-var playerDamage = 0;
-var enemyDamage = 0;
-var playerTotalDamage = 0;
-var enemyTotalDamage = 0;
+var player1Damage = 0;
+var player2Damage = 0;
 
 function combat() {
-  enemyHealth = enemyHealth - playerDamage;
-  playerHealth = playerHealth - enemyDamage;
+  player1Damage = currentPlayer1.class.damage
+  player2Damage = currentPlayer2.class.damage
+  player2Health = player2Health - player1Damage;
+  player1Health = player1Health - player2Damage;
   combatValidation()
 }
